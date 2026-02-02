2087b94f9cbbc7179de2bdf1bfb74fb17be8c3c8
imannfazal <imannfazal@gmail.com>
2026-02-02 19:38:10 +0500

    Implement JWT authentication with register and login

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,14 @@
 from fastapi import FastAPI
 from app.database import engine, Base
 from app.models import user, role
+from app.routers import auth
+
+app = FastAPI(title="B2B Wholesale API")
 
 Base.metadata.create_all(bind=engine)
 
-app = FastAPI(title="B2B Wholesale API")
+app.include_router(auth.router)
+
 
 @app.get("/")
 def health_check():
