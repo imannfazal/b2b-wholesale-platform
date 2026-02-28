7f4e80a0543de9fc4872961dc4d0f6e9b5062de5
imannfazal <imannfazal@gmail.com>
2026-02-28 20:25:15 +0500

    CORS for frontend integration

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,7 @@
 # app/main.py
 from fastapi import FastAPI
 from app.database import engine, Base
+from fastapi.middleware.cors import CORSMiddleware
 
 # Import models (only needed for metadata creation)
 from app.models import user as user_model, product as product_model
@@ -9,6 +10,17 @@ from app.models import user as user_model, product as product_model
 from app.routers import auth, user, product, order
 
 app = FastAPI(title="B2B Wholesale API")
+
+origins = [
+    "http://localhost:5173",
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Create all tables
 Base.metadata.create_all(bind=engine)
